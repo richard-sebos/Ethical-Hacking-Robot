9d869d8d0e431024d1cf390076e3c6ea46609103
Richard Chamberlain <info@sebostechnology.com>
2025-03-10 06:52:46 -0600

    Create setup_config.sh

---
+++ Git-Automation/setup_config.sh
@@ -0,0 +1,6 @@
+USERNAME=<Linux User to Create>
+USER_HOME="/home/$USERNAME"
+GITHUB_REPO="git@github.com:<git user>/<repo>"
+GIT_USER_NAME=
+GIT_USER_EMAIL=
+GITHUB_API_TOKEN=$(sudo cat /home/richard/.github_token 2>/dev/null)
